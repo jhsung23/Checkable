7fb78e4fbc896934b9ca4c31cfe3f920dd8b4036
seeyeon <71398979+seeyeon@users.noreply.github.com>
2022-11-09 17:36:01 +0900

    feat.온보딩 시작 페이지와 병원찾기 페이지 구현

---
+++ app/src/main/java/com/example/checkable1/OnBoarding_HospitalPage.java
@@ -0,0 +1,14 @@
+package com.example.checkable1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnBoarding_HospitalPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding_hospital_page);
+    }
+}
