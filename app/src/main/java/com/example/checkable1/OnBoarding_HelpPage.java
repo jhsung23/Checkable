8468809e41f357c6752dd20c119444fab8536c95
cyeon1118 <78338944+cyeon1118@users.noreply.github.com>
2022-11-09 20:08:35 +0900

    Feat : onBoarding help page

---
+++ app/src/main/java/com/example/checkable1/OnBoarding_HelpPage.java
@@ -0,0 +1,14 @@
+package com.example.checkable1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnBoarding_HelpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding_help_page);
+    }
+}
