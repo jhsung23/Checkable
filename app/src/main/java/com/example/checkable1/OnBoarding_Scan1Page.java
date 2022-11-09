5e9c55a4fc68d886b73438d8542852083af695e0
cyeon1118 <78338944+cyeon1118@users.noreply.github.com>
2022-11-09 20:13:25 +0900

    style : xml modified

--- app/src/main/java/com/example/checkable1/OnBoarding_Scan1Page.java
+++ app/src/main/java/com/example/checkable1/OnBoarding_Scan1Page.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class OnBoarding_Scan1 extends AppCompatActivity {
+public class OnBoarding_Scan1Page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
